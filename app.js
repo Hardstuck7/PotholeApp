2f0d471e53761179d6e2f430c28e58ad6ad75a8d
Hardstuck7 <mwjsharpe@gmail.com>
2022-05-11 14:24:38 +1000

    works ish

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 // ROUTES
 app.get('/', (req,res) => {
     res.send({
-        center: { lat: -28.64, lng: 153.61 },
-        zoom: 12,
-        mapTypeId: 'hybrid',
+        "lat": -28.64,
+        "lng": 153.61,
+        "pic": "goofy aah.png"
     });
 });
 
