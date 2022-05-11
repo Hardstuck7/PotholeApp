3c6f5433fe30005837ba45266a2baf91ef9079f4
Hardstuck7 <mwjsharpe@gmail.com>
2022-05-11 14:02:44 +1000

    server worksish
    inputing the wrong thing

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 // ROUTES
 app.get('/', (req,res) => {
-    res.send('we are at home');
+    res.send({
+        center: { lat: -28.64, lng: 153.61 },
+        zoom: 12,
+        mapTypeId: 'hybrid',
+    });
 });
 
 app.listen(5500);
