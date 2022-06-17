f87ead0e5e900bf1c0f47fa62feee89714a48081
Hardstuck7 <mwjsharpe@gmail.com>
2022-06-17 12:21:03 +1000

    nothing works, pain

--- Routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const pothole = require("../Controller/controller.js");
-
-// Retrieve all potholes
-router.get("/", pothole.findAll);
-
-
-module.exports = router
